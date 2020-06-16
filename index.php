132efb7149c8a1e629cf81ae7f254eb9ca0aea6d
Stefano <stefano2890@gmail.com>
2020-06-16 15:52:38 +0200

    files created

---
+++ index.php
@@ -0,0 +1,6 @@
+<!-- pagina faq di Google
+Create un array per salvare tutte le faq che trovate in questa pagina:
+https://policies.google.com/faq
+Stampate in pagina tutte le domande dinamicamente con un ciclo in php.
+BONUS: stilare la pagina
+Nome repo: php-google-faq -->
