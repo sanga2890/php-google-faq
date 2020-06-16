0b856ded64888e3b03176e19f6282f894d236f06
Stefano <stefano2890@gmail.com>
2020-06-16 16:46:15 +0200

    creato html dinamico

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ Nome repo: php-google-faq -->
 
 <!-- creo l'array policies in cui inserirò le domande -->
 <?php
+
 $policies = [
 
     [
@@ -42,12 +43,48 @@ $policies = [
         "answer" => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
     ]
 ];
-
-    foreach ($policies as $faq) {
-        echo $faq['question'];
-        echo "<br><br>";
-        echo $faq['answer'];
-        echo "<br><br><br>";
-    }
+    // // ciclo l'array con un ciclo foreach e stampo in pagina con echo;
+    // foreach ($policies as $faq) {
+    //     echo $faq['question'];
+    //     echo "<br><br>";
+    //     echo $faq['answer'];
+    //     echo "<br><br><br>";
+    // }
 
 ?>
+
+
+<!-- Creo la struttura Html per stampare in pagina -->
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Domande frequenti - Privacy e termini - Google</title>
+    </head>
+    <body>
+
+        <header>
+
+        </header>
+
+        <main>
+            <div class="container">
+                <?php foreach ($policies as $faq) { ?>
+                <div class="faq">
+                    <h3 class="question">
+                        <?php echo $faq['question']; ?>
+                    </h3>
+                    <h4 class="answer">
+                        <?php echo $faq['answer']; ?>
+                    </h4>
+
+                </div>
+                <?php
+                }
+                ?>
+            </div>
+        </main>
+
+    </body>
+</html>
